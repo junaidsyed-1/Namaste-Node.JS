40ca8d4a2039ef88f7ef98a6b4986961b70d52f9
junaidsyed-1 <jaanusd79@gmail.com>
2024-08-28 20:29:09 +0530

    globalThis ECMAstandard

---
+++ app.js
@@ -0,0 +1,5 @@
+// Node js provides a global object and it is not a part of V8, It is inside Node js like there are more super powers other than V8.
+
+// console.log(global)
+
+console.log(globalThis)
