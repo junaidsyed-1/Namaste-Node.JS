cfc936a73b799b032e5210f0d8a40604e69170c3
junaidsyed-1 <jaanusd79@gmail.com>
2024-10-15 19:24:57 +0530

    connecting to DB using mongoDB npm

---
+++ database.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require('mongodb');
+
+
+const URI = "mongodb+srv://jaanusd79:lh5qjOBerdWCXQVW@namastenode.nud25.mongodb.net/"
+
+const client = new MongoClient(URI);
+
+// Database name
+const dbName = "NamasteNode";
+
+async function main() {
+    await client.connect();
+    console.log("Connected to MongoDB server");
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+
+    const data = {
+        firstName : "Junaid",
+        lastName: "Syed",
+        city : "Delhi",
+        phone: "931294802"
+    }
+
+    const insertRes = await collection.insertOne(data);
+    console.log(("Inserted Data =>", insertRes))
+
+    return "done.."
+};
+
+main()
+   .then(console.log)
+   .catch(console.error)
+   .finally(() => client.close());
