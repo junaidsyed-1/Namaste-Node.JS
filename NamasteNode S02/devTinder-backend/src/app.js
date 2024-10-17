724b49529b5b92369fdb858daa308b9c7565bd49
junaidsyed-1 <jaanusd79@gmail.com>
2024-10-17 22:21:57 +0530

    created express server

--- NamasteNode S02/devTinder-backend/src/app.js
+++ NamasteNode S02/devTinder-backend/src/app.js
@@ -3,9 +3,14 @@ const port = 7777;
 
 const app = express();
 
-app.use((req,res) => {
-    res.send("Hello world!")
-})
+app.use( "/dash" ,(req,res) => {
+    res.send("Weclome to the Dashboard!")
+});
+
+app.use( "/test" ,(req,res) => {
+    res.send("Test page!")
+});
+
 
 app.listen(port, ()=>{
     console.log("app is successfully listening to port:", port);
