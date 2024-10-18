27b528315edde5a275051521034d12f9ae23d07c
junaidsyed-1 <jaanusd79@gmail.com>
2024-10-18 19:45:53 +0530

    Middleware

--- NamasteNode S02/devTinder-backend/src/app.js
+++ NamasteNode S02/devTinder-backend/src/app.js
@@ -3,18 +3,20 @@ const port = 7777;
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling route 1");
-    next();
-    res.send("respone!");
-  },
-  (req, res) => {
-    console.log("Handling route 2");
-    res.send("RESPONSE 2!!");
-  }
-);
+app.use("/user", (req, res, next) => {
+  console.log("Middleware: 1 , Handling the route 1");
+  next();
+});
+
+app.get("/user", (req, res, next) => {
+  console.log("Middleware: 2, Handling the route 2");
+  next();
+});
+
+app.get("/user", (req, res) => {
+  console.log("Actual route handler");
+  res.send("Response");
+});
 
 app.listen(port, () => {
   console.log("app is successfully listening to port:", port);
