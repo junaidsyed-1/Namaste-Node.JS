59ccf2c4db47d559a33953852c88f708a17cbe91
junaidsyed-1 <jaanusd79@gmail.com>
2024-10-27 14:00:05 +0530

    created /user/connections API

--- NamasteNode S02/devTinder-backend/src/routes/user.js
+++ NamasteNode S02/devTinder-backend/src/routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 
+const USER_DATA = "firstName lastName age gender photoUrl about";
+
 router.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -15,7 +17,44 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
       "firstName lastName age skills gender photoUrl about"
     );
 
+    if (connectionRequest.length === 0) {
+      return res.status(404).json({ message: "No connection requests found" });
+    }
+
     res.json({ message: "Data fetched Successfuly", data: connectionRequest });
+  } catch (error) {
+    res.status(400).json({ ERROR: error.message });
+  }
+});
+
+router.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    if (!loggedInUser) {
+      return res.json({ message: "Invalid Id" });
+    }
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_DATA)
+      .populate("toUserId", USER_DATA);
+
+    if (!connectionRequest || connectionRequest.length === 0) {
+      return res.status(404).json({ message: "No connections found" });
+    }
+
+    const data = connectionRequest.map((row) => {
+      if (row.toUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.fromUserId;
+      }
+      return row.toUserId;
+    });
+
+    res.json({ message: "User data", data });
   } catch (error) {
     res.status(400).json({ ERROR: error.message });
   }
