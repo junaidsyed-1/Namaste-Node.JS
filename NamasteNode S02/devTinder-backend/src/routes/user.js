c49941cbf3b04ad9d250a6708393782f0c5694a5
junaidsyed-1 <jaanusd79@gmail.com>
2024-11-04 13:40:49 +0530

    Made pagination in Feed API

--- NamasteNode S02/devTinder-backend/src/routes/user.js
+++ NamasteNode S02/devTinder-backend/src/routes/user.js
@@ -61,6 +61,10 @@ router.get("/user/connections", userAuth, async (req, res) => {
 router.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
 
     const connectionRequest = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -77,7 +81,10 @@ router.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_DATA);
+    })
+      .select(USER_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.json({ data: user });
   } catch (error) {}
