e8f1352df2f2cb86d2ba1722f43a1e5c57d9d292
junaidsyed-1 <jaanusd79@gmail.com>
2024-11-06 19:32:06 +0530

    Two ways of routing

--- NamasteNode S02/devTinder-frontend/src/App.jsx
+++ NamasteNode S02/devTinder-frontend/src/App.jsx
@@ -1,7 +1,43 @@
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <>
-      <h1 className="bg-red-200">Hello World</h1>
+      {/* <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+      <RouterProvider router={router} />
     </>
   );
 }
